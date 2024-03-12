b00c4f5bbca925d146e8a2d96788384dca211836
amznrmh <117199325+amznrmh@users.noreply.github.com>
2024-03-12 00:23:02 +0000

    community post feature

---
+++ SafeSpace/Components/CommunityPostView.swift
@@ -0,0 +1,18 @@
+//
+//  CommunityPostView.swift
+//  SafeSpace
+//
+//  Created by Aman Narmah on 12/03/2024.
+//
+
+import SwiftUI
+
+struct CommunityPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommunityPostView()
+}
